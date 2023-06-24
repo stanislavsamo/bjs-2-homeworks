26d4cd462156788e7ce09d562e930a78f476ee72
stanislavsamo <f11samokhin@gmail.com>
2023-06-24 22:03:38 +0500

    «first»

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,5 +1,17 @@
 function getArrayParams(...arr) {
+  let min = Infinity;
+  let max = -Infinity;
+  let sum = 0;
 
+  for (let i = 0; i < arr.length; i++) {
+    const current = arr[i];
+    max = Math.max(max, current);
+    min = Math.min(min, current);
+    sum += current;
+  }
+
+  const avg = +(sum / arr.length).toFixed(2);
+  
   return { min: min, max: max, avg: avg };
 }
 
