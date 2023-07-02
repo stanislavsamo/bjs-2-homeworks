e038cb65493c63cff6da161d08dcc434df5bf0f7
stanislavsamo <f11samokhin@gmail.com>
2023-07-02 20:41:32 +0500

    Решение задания №4

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,37 @@
 function Student(name, gender, age) {
-  
+	this.name = name;
+	this.gender = gender;
+	this.age = age;
+	this.marks = [];
 }
 
-Student.prototype.setSubject = function (subjectName) {
-  
+Student.prototype.setSubject = function(subjectName) {
+	this.subject = subjectName;
+};
+
+Student.prototype.addMarks = function(...marks) {
+	if (!this.marks) {
+		console.log('Студент отчислен и не имеет оценок.');
+		return;
+	}
+
+	this.marks.push(...marks);
 }
 
-Student.prototype.addMarks = function (...marks) {
-  
+Student.prototype.getAverage = function() {
+	if (!this.marks || this.marks.length === 0) {
+		return 0;
+	}
+
+	let sum = this.marks.reduce((acc, mark) => {
+		return acc + mark;
+	}, 0);
+
+	return sum / this.marks.length;
 }
 
-Student.prototype.getAverage = function () {
-  
-}
-
-Student.prototype.exclude = function (reason) {
-  
+Student.prototype.exclude = function(reason) {
+	delete this.subject;
+	delete this.marks;
+	this.excluded = reason;
 }
