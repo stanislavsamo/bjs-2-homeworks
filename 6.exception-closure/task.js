a4fc77aa0a683ca0f7c33083d6fc5a0bea76333e
stanislavsamo <f11samokhin@gmail.com>
2023-07-16 19:50:36 +0500

    Solving 6.1.1

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,7 @@
-﻿
+﻿function parseCount(value) {
+	let parsedValue = Number.parseFloat(value);
+	if (Number.isNaN(parsedValue)) {
+		throw new Error('Невалидное значение');
+	}
+	return parsedValue;
+}
