f13126a19f7904da8c5c3eb6f1a14991433b0ae8
stanislavsamo <f11samokhin@gmail.com>
2023-07-16 22:38:13 +0500

    Solving 6.2.4

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -15,37 +15,42 @@ function validateCount(value) {
 }
 
 class Triangle {
-	constructor(a, b, c) {
-		if (a + b <= c || b + c <= a || a + c <= b) {
-			throw new Error('Треугольник с такими сторонами не существует');
-		}
-		this.a = a;
-		this.b = b;
-		this.c = c;
-	}
-
-	perimeter() {
-		return this.a + this.b + this.c;
-	}
-
-	area() {
-		const s = this.perimeter() / 2;
-		const areaSquared = s * (s - this.a) * (s - this.b) * (s - this.c);
-		return Math.sqrt(areaSquared).toFixed(3);
-	}
-}
-
-function getTriangle(a, b, c) {
-	try {
-		return new Triangle(a, b, c);
-	} catch (error) {
-		return {
-			get area() {
-				return 'Ошибка! Треугольник не существует';
-			},
-			get perimeter() {
-				return 'Ошибка! Треугольник не существует';
-			},
-		};
-	}
-}
+    constructor(a, b, c) {
+      if (a + b <= c || a + c <= b || b + c <= a) {
+        throw new Error('Треугольник с такими сторонами не существует');
+      }
+      this.a = a;
+      this.b = b;
+      this.c = c;
+    }
+  
+    get perimeter() {
+      return this.a + this.b + this.c;
+    }
+  
+    set perimeter(value) {
+      throw new Error('Невозможно переопределить значение периметра треугольника.');
+    }
+  
+    get area() {
+      const p = this.perimeter / 2;
+      const area = Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c));
+      return parseFloat(area.toFixed(3)); // Return the area as a numeric value, not a string
+    }
+  
+    set area(value) {
+      throw new Error('Невозможно переопределить значение площади треугольника.');
+    }
+  }
+  
+  function getTriangle(a, b, c) {
+    try {
+      return new Triangle(a, b, c);
+    } catch (error) {
+      return {
+        area: () => 'Ошибка! Треугольник не существует', // Use a function to return the error message as a string
+        perimeter: () => 'Ошибка! Треугольник не существует', // Use a function to return the error message as a string
+      };
+    }
+  }
+  
